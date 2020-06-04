84ab29730a9cf0d68abe05f1197e28e7006ae4b8
Brian Tran <98brian.tran@gmail.com>
2020-06-04 22:25:21 +0000

    fixed some bugs

--- fetcher.js
+++ fetcher.js
@@ -1,42 +1,26 @@
 const request = require('request');
-const readline = require('readline');
 const fs = require('fs');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
 
 const fetcher = () => {
   arg = process.argv.slice(2);
-  const path = arg[1];
-  let doneReading = false;
-  let buffer;
-  request(arg[0], (error, response, body) => {
-    if (error) throw `could not open file: ${error}`; // Print the error if one occurred
-    console.log('statusCode:', response.statusCode); // Print the response status code if a response was received
-    buffer = new Buffer(body); // Print the HTML for the Google homepage.
-    if (response.statusCode === 200) {
-      exists(path, buffer);
-    }
-  });
+  if(arg[1].split('.').includes('html')) {
+    const path = arg[1];
+    let buffer;
+    request(arg[0], (error, response, body) => {
+      if (error) throw `could not open file: ${error}`; // Print the error if one occurred
+      console.log('statusCode:', response.statusCode); // Print the response status code if a response was received
+      buffer = new Buffer(body); // Print the HTML for the Google homepage.
+      if (response.statusCode === 200) {
+        downloadContent(path, buffer);
+      }
+    });
+  } else {
+    throw `Error not a valid valid path`
+  }
 };
 
-const exists = (path, content) => {
-  fs.stat(path, (err, stats) => {
-    if (err) throw `couldn't check if file exist: ${err}`;
-    if (stats.isFile()) {
-      rl.question('File already exisits! Would you like to overwrite file? [y/n]', (answer) => {
-        if (answer === 'y') {
-          rl.close();
-          downloadContent(path, content)
-        }
-        rl.close();
-      });
-    }
-  });
-};
 
 const downloadContent = (path, content) => {
   fs.open(path, 'w', (err, fd) => {
